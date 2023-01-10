9348475d6a81904232a6a5c6b3c05e5125321e39
Basile Carle <basile.carle@edhec.com>
2023-01-10 12:46:40 +0100

    création box info générales

---
+++ frontend/src/components/GeneralInfo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import basile from "@assets/basileCarle.png";
+
+function GeneralInfo() {
+  return (
+    <div className="general-info">
+      <img src={basile} alt="Basile Carle" id="basile" />
+    </div>
+  );
+}
+
+export default GeneralInfo;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 function Navbar() {
   return (
-    <section className="navbar">
-      <strong className="title">
-        <div>Basile CARLE</div>
-        <div>Développeur junior Fullstack</div>
-      </strong>
-    </section>
+    <div className="container">
+      <section className="navbar">
+        <strong className="title">
+          <div>Basile CARLE</div>
+          <div>Développeur junior Fullstack</div>
+        </strong>
+      </section>
+    </div>
   );
 }
 
