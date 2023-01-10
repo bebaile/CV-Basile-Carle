6fe8c1759a66581184cd0f7e71e29f847953400a
Basile Carle <basile.carle@edhec.com>
2023-01-10 17:05:46 +0100

    création de la zone graphique mais pas encore d'implémentation de l'API

---
+++ frontend/src/components/GithubSummary.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function GithubSummary() {
+  return (
+    <div className="container">
+      <div className="github-summary">
+        Résumé de mon profil Github : à implémenter plus tard
+      </div>
+    </div>
+  );
+}
+
+export default GithubSummary;
