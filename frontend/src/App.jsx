6294ef0975fa619d38ecca93420d67c059a80b20
Basile Carle <basile.carle@edhec.com>
2023-01-10 12:19:39 +0100

    version simple avec titre et dimensions adaptées au mobile et affichage dynamique

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
-import Home from "./pages/Home";
+import React from "react";
+import Navbar from "@components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <section className="navbar">
+      <strong className="title">
+        <div>Basile CARLE</div>
+        <div>Développeur junior Fullstack</div>
+      </strong>
+    </section>
+  );
+}
+
+export default Navbar;
