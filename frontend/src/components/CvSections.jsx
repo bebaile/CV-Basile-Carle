ce89c121352379ced44fd5b1dbec11af33e970e6
Basile Carle <basile.carle@edhec.com>
2023-01-11 11:55:38 +0100

    rempli contenu sections compétences

--- frontend/src/components/CvSections.jsx
+++ frontend/src/components/CvSections.jsx
@@ -10,6 +10,52 @@ function CvSections() {
           <div>Formations</div>
           <div>Extra-professionel</div>
         </div>
+        <div className="section-content-container">
+          <div className="competences">
+            <article>
+              <div className="sub-section-title">Développement Web</div>
+              <div className="sub-section-content">
+                Maquettage, développement composants frontend
+                (HTML/CSS/JavaScript + React) et backend (Node/Express/MySQL) +
+                intégration & développement d’API REST + bases C
+              </div>
+            </article>
+            <article>
+              <div className="sub-section-title">Conseil & Recherche</div>
+              <div className="sub-section-content">
+                Gestion de projet, réalisation d’études et prévisions de marché,
+                modélisation, conduite d’entretien, recherche avancée,
+                sensibilité à l’expérience utilisateur, esprit rigoureux,
+                créatif et collaboratif
+              </div>
+            </article>
+            <article>
+              <div className="sub-section-title">Anglais</div>
+              <div className="sub-section-content">
+                Courant, lu, écrit, parlé, TOEIC : 860/990 et pratique
+                quotidienne en milieu professionnel (conférences, tables rondes,
+                interviews, recherche documentaire ...)
+              </div>
+            </article>
+            <article>
+              <div className="sub-section-title">Allemand</div>
+              <div className="sub-section-content">
+                Lu, écrit, parlé (séjour linguistique à Hanovre)
+              </div>
+            </article>
+            <article>
+              <div className="sub-section-title">Informatique</div>
+              <div className="sub-section-content">
+                Word, Excel, PowerPoint, Access, Photoshop, GIMP, Lightroom
+                (développement photo), Mac, PC et Unix/Linux, compétences
+                réseaux, domotique
+              </div>
+            </article>
+          </div>
+          <div className="experiences-professionnelles">xxxxx</div>
+          <div className="formations">xxxxx</div>
+          <div className="Extra-professionel">xxxxx</div>
+        </div>
       </div>
     </div>
   );
