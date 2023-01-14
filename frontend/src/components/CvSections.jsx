14b5d556775d3e8b31e5dc7ebc0a66812cfafd8e
Basile Carle <puma@iMac-de-Basile.lan>
2023-01-14 16:01:03 +0100

    ajout indicateur d'onglet sélectionné pour la section

--- frontend/src/components/CvSections.jsx
+++ frontend/src/components/CvSections.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import sections from "../data/section.json";
 
 function CvSections() {
+  const [isSelected, setIsSelected] = useState(1);
+
   const handleOnglet = (index) => {
-    console.error(document.querySelector(".section-slider").classList.value);
     const slider = document.querySelector(".section-slider");
     switch (index) {
       case 1:
@@ -11,24 +12,28 @@ function CvSections() {
         slider.classList.remove("formations-selected");
         slider.classList.remove("extraprofessionel-selected");
         slider.classList.add("competences-selected");
+        setIsSelected(1);
         break;
       case 2:
         slider.classList.remove("competences-selected");
         slider.classList.remove("formations-selected");
         slider.classList.remove("extraprofessionel-selected");
         slider.classList.add("experiences-selected");
+        setIsSelected(2);
         break;
       case 3:
         slider.classList.remove("competences-selected");
         slider.classList.remove("experiences-selected");
         slider.classList.remove("extraprofessionel-selected");
         slider.classList.add("formations-selected");
+        setIsSelected(3);
         break;
       case 4:
         slider.classList.remove("competences-selected");
         slider.classList.remove("experiences-selected");
         slider.classList.remove("formations-selected");
         slider.classList.add("extraprofessionel-selected");
+        setIsSelected(4);
         break;
       default:
         break;
@@ -40,7 +45,11 @@ function CvSections() {
       <div className="cv-sections">
         <div className="onglet">
           <div
-            className="competences-onglet"
+            className={
+              isSelected === 1
+                ? "competences-onglet onglet-selected"
+                : "competences-onglet"
+            }
             role="button"
             onClick={() => handleOnglet(1)}
             onKeyDown={() => handleOnglet(1)}
@@ -48,8 +57,13 @@ function CvSections() {
           >
             Compétences
           </div>
+
           <div
-            className="experiences-onglet"
+            className={
+              isSelected === 2
+                ? "experiences-onglet onglet-selected"
+                : "experiences-onglet"
+            }
             role="button"
             onClick={() => handleOnglet(2)}
             onKeyDown={() => handleOnglet(2)}
@@ -58,7 +72,11 @@ function CvSections() {
             Expériences professionelles
           </div>
           <div
-            className="formations-onglet"
+            className={
+              isSelected === 3
+                ? "formations-onglet onglet-selected"
+                : "formations-onglet"
+            }
             role="button"
             onClick={() => handleOnglet(3)}
             onKeyDown={() => handleOnglet(3)}
@@ -67,7 +85,11 @@ function CvSections() {
             Formations
           </div>
           <div
-            className="extraprofessionel-onglet"
+            className={
+              isSelected === 4
+                ? "extraprofessionel-onglet onglet-selected"
+                : "extraprofessionel-onglet"
+            }
             role="button"
             onClick={() => handleOnglet(4)}
             onKeyDown={() => handleOnglet(4)}
