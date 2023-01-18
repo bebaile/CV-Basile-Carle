241d1ddf0c17034270842c3f472b2db8e7f684fb
Basile Carle <basile.carle@edhec.com>
2023-01-18 17:06:41 +0100

    mise en place de l'interface graphique pour la connection

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import api from "@services/services";
+import "@styles/login.css";
+import basile from "@assets/basileCarle.png";
 
 function Login() {
   const ENDPOINT = "/users";
@@ -8,7 +10,40 @@ function Login() {
       console.error(result);
     });
   }, []);
-  return <div>coucou</div>;
+  return (
+    <div className="container">
+      <div className="login-section">
+        <div className="login-box">
+          <div id="user-photo">
+            <img
+              src={basile}
+              alt="Basile, l'administrateur unique de ce site"
+              id="basile"
+            />
+          </div>
+          <div id="connexion-form">
+            <form>
+              <label htmlFor="login">
+                <div>Login :</div>
+                <div>
+                  <input type="text" id="login" name="login" />
+                </div>
+              </label>
+              <label htmlFor="password">
+                <div>Mot de passe :</div>
+                <div>
+                  <input type="password" id="password" name="password" />
+                </div>
+              </label>
+              <label htmlFor="submit">
+                <input type="submit" id="submit" className="connect-btn" />
+              </label>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -11,6 +12,7 @@ export default defineConfig({
       "@components": path.resolve(__dirname, "src/components"),
       "@pages": path.resolve(__dirname, "src/pages"),
       "@services": path.resolve(__dirname, "src/services"),
+      "@styles": path.resolve(__dirname, "src/styles"),
     },
   },
 });
